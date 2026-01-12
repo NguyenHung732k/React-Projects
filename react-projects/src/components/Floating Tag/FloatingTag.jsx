dd56811dbe27561d6ecc31bf3ea692d5d0d90a8a
NguyenHung732k <code.nhnguyen@gmail.com>
2026-01-12 23:11:07 +0700

    Commit Floating Tag

--- react-projects/src/components/Floating Tag/FloatingTag.jsx
+++ react-projects/src/components/Floating Tag/FloatingTag.jsx
@@ -9,6 +9,7 @@ const FloatingTag = () => {
                 <div className="tag bg-teal-500">Tag 2</div>
                 <div className="tag bg-orange-500">Tag 3</div>
                 <div className="tag bg-pink-500">Tag 4</div>
+                <div className="tag bg-pink-500">Tag 5</div>
             </div>
         </div>
     )
